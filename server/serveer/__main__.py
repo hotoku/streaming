24448fad65f63ebea2efa8a76a25123031b67134
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-08-03 22:02:28 +0900

    echo

--- server/serveer/__main__.py
+++ server/serveer/__main__.py
@@ -1,14 +1,21 @@
-#!/usr/bin/env python
-
 import logging
+from typing import Any, Dict
+import json
+
 import click
+from flask import Flask, request
 
-LOGGER = logging.getLogger(__file__)
+LOGGER = logging.getLogger(__name__)
+app = Flask(__name__)
 
+@app.route("/echo", methods=["post"])
+def echo() -> bytes:
+    data: Dict[str, Any] = request.get_json() # type: ignore
+    return json.dumps(data).encode("utf-8")
 
 @click.command()
 def main():
-    print("main")
+    app.run(debug=True, host="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
     main()
