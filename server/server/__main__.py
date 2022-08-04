acd766545cd724faf6d33eab4cc98f620861f2f1
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-08-04 16:39:44 +0900

    fix

--- server/server/__main__.py
+++ server/server/__main__.py
@@ -27,7 +27,7 @@ def setup_logger(debug: bool = False):
 
 
 @app.route("/videos", methods=["get"])
-def videos() -> List[Dict[int, str]]:
+def videos() -> list[dict[str, Any]]:
     con = get_con()
     cur = con.cursor()
     data = cur.execute("""
@@ -37,7 +37,7 @@ def videos() -> List[Dict[int, str]]:
     from
       videos
     """)
-    return [dict(id=row["id"], name=row["name"]) for row in data]  # type: ignore
+    return [dict(id=row["id"], name=row["name"]) for row in data]
 
 
 @app.route("/echo", methods=["post"])
