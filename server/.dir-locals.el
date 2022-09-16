5d421792b0146e05ab90ec8c7ee1debecffd3f63
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-16 17:28:35 +0900

    conftest

---
+++ server/.dir-locals.el
@@ -0,0 +1,6 @@
+(
+ (python-mode . ((lsp-pyright-venv-path . "/Users/hotoku/Library/Caches/pypoetry/virtualenvs/streaming-server-LfQdTTwB-py3.9")))
+)
+
+;; list[tuple[モード名, list[tuple[変数名, 値]]]]という型
+;; ネストが面倒なので注意
---
+++ server/test_server/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+from xprocess import ProcessStarter
+from flask import Flask
+from flask.testing import FlaskClient
+
+import server as _server
+
+@pytest.fixture
+def app():
+    app = _server.app
+    app.config.update({
+        "TESTING": True
+    })
+    yield app
+
+@pytest.fixture
+def client(app: Flask) -> FlaskClient:
+    return app.test_client()
+
+@pytest.fixture
+def server(xprocess):
+    port = 8080
+    server_name = "myserver"
+
+    class Starter(ProcessStarter):
+        pattern = r"Running on http:"  # type: ignore
+        timeout = 2
+        terminate_on_interrupt = True
+        args = ["python",
+                "-m", "di_predict", "server",
+                "--port", port]  # type: ignore
+    logfile = xprocess.ensure(server_name, Starter)
+    yield dict(url="http://localhost", port=port, logfile=logfile)
+
+    xprocess.getinfo(server_name).terminate()
