f4d453dd9c006efeb2b4c122ab5411bc632d8ef3
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-16 16:18:20 +0900

    thumbnail

--- server/thumbnail/__main__.py
+++ server/thumbnail/__main__.py
@@ -1,18 +1,31 @@
-import json
+import pathlib as pl
+import subprocess as sp
+import os
 
 import click
+import luigi
 
-
-def convert(d: str):
-    pass
+from .tasks import Thumbnail
 
 
 @click.command()
-@click.argument("config", type=click.Path(exists=True, dir_okay=False, readable=True))
-def main(config: str):
-    with open(config) as f:
-        cfg = json.load(f)
-    print(cfg)
+@click.argument("root-dir", type=click.Path(exists=True, file_okay=False))
+@click.argument("dest-root", type=click.Path(file_okay=False))
+@click.option("--extension", type=str, default="mp4")
+def main(root_dir: str, dest_root: str,
+         extension: str):
+    if not os.path.exists(dest_root):
+        os.makedirs(dest_root)
+    files = pl.Path(root_dir).glob(f"./**/*.{extension}")
+    tasks = [
+        Thumbnail(str(file), dest_root)
+        for file in files
+    ]
+    luigi.build(
+        tasks,
+        local_scheduler=True,
+        workers=4
+    )
 
 
 if __name__ == "__main__":
---
+++ server/thumbnail/tasks.py
@@ -0,0 +1,52 @@
+import pathlib as pl
+from hashlib import sha256
+import subprocess as sp
+import luigi
+
+
+class ThumbnailConvertError(Exception):
+    pass
+
+
+def make_thumbnail(video: str, thumb: str) -> None:
+    ret = sp.run(
+        [
+            "ffmpeg",
+            "-i",
+            video,
+            "-ss",
+            "00:00:00.010",
+            "-y",
+            "-vframes",
+            "1",
+            thumb
+        ],
+        stdout=sp.PIPE,
+        stderr=sp.PIPE,
+        text=True
+    )
+    if ret.returncode != 0:
+        raise ThumbnailConvertError(
+            f"stdout={ret.stdout}, stderr={ret.stderr}")
+
+
+class Thumbnail(luigi.Task):
+    src_path: str = luigi.Parameter()  # type: ignore
+    dest_root: str = luigi.Parameter(significant=False)  # type: ignore
+
+    @staticmethod
+    def dest_path(src_path: str, dest_root: str) -> str:
+        ret = pl.Path(dest_root) / \
+            (sha256(src_path.encode()).hexdigest() + ".jpg")
+        return str(ret.resolve())
+
+    def output(self) -> list[luigi.LocalTarget]:
+        return [
+            luigi.LocalTarget(self.dest_path(self.src_path,
+                                             self.dest_root))
+        ]
+
+    def run(self):
+        make_thumbnail(self.src_path,
+                       self.dest_path(self.src_path,
+                                      self.dest_root))
