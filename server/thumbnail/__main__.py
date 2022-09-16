af731b19161dfea417f75cf0ed9c69865cbb2459
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-16 18:05:09 +0900

    thumbnail: root dir become list

--- server/thumbnail/__main__.py
+++ server/thumbnail/__main__.py
@@ -1,23 +1,61 @@
+from typing import Any, Optional
+import re
 import pathlib as pl
 import os
+import json
 
 import click
+from click.core import Context, Parameter
+from click.types import ParamType
 import luigi
 
 from .tasks import Thumbnail
 
 
+class StrList(ParamType):
+    name = "list of strings"
+
+    def convert(self,
+                value: Any,
+                param: Optional[Parameter],
+                ctx: Optional[Context]) -> Any:
+        param, ctx
+        if type(value) is list:
+            return value
+        if type(value) is str:
+            return value.split(",")
+        return []
+
+    def __call__(self,
+                 value: Any,
+                 param: Optional[Parameter] = None,
+                 ctx: Optional[Context] = None) -> Any:
+        if value is None:
+            return None
+        return self.convert(value, param, ctx)
+
+
+def make_relative(root: str, path: str) -> str:
+    p1 = str(pl.Path(root).resolve())
+    p2 = str(pl.Path(path).resolve())
+    assert p2.startswith(p1)
+    return os.path.join(root, re.sub(f"^{p1}/", "", p2))
+
+
 @click.command()
-@click.argument("root-dir", type=click.Path(exists=True, file_okay=False))
+@click.argument("root-dirs", type=StrList())
 @click.argument("dest-root", type=click.Path(file_okay=False))
+@click.option("--json-path", type=click.Path(dir_okay=False), default=".info.json")
 @click.option("--extension", type=str, default="mp4")
-def main(root_dir: str, dest_root: str,
+def main(root_dirs: str, dest_root: str, json_path: str,
          extension: str):
     if not os.path.exists(dest_root):
         os.makedirs(dest_root)
-    files = pl.Path(root_dir).glob(f"./**/*.{extension}")
+    files = [
+        str(f) for d in root_dirs for f in pl.Path(d).glob(f"./**/*.{extension}")
+    ]
     tasks = [
-        Thumbnail(str(file), dest_root)
+        Thumbnail(file, dest_root)
         for file in files
     ]
     luigi.build(
@@ -25,6 +63,15 @@ def main(root_dir: str, dest_root: str,
         local_scheduler=True,
         workers=4
     )
+    info = [
+        dict(
+            video=f,
+            thumbnail=make_relative(
+                dest_root, Thumbnail.dest_path(f, dest_root))
+        )
+        for f in files
+    ]
+    json.dump(info, open(json_path, "w"))
 
 
 if __name__ == "__main__":
