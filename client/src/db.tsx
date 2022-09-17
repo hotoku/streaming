4049fdfe20d0ed2e324f7ac0ccce388f6539d693
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-18 02:57:19 +0900

    fix

--- client/src/db.tsx
+++ client/src/db.tsx
@@ -3,6 +3,5 @@ import { Video } from "./types/Video";
 export const fetchVideoList = async (callback: (vs: Video[]) => void) => {
   const response = await fetch("/video/list");
   const data: Video[] = await response.json();
-  // callback(data);
-  callback(data.slice(0, 1));
+  callback(data);
 };
