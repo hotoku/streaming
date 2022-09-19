6ee7e0f406df211c066e2a2ae88fe3c493964927
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-19 10:30:57 +0900

    fix test

--- server/test_server/conftest.py
+++ server/test_server/conftest.py
@@ -1,14 +1,7 @@
+from server import app as APP
 import pytest
-from xprocess import ProcessStarter
 from flask import Flask
 from flask.testing import FlaskClient
-
-import sys
-open("/tmp/hoge", "w").write(str(sys.path))
-
-from server import app as APP
-
-
 
 
 @pytest.fixture
@@ -22,19 +15,3 @@ def app():
 @pytest.fixture
 def client(app: Flask) -> FlaskClient:
     return app.test_client()
-
-
-@pytest.fixture
-def server(xprocess):
-    server_name = "myserver"
-
-    class Starter(ProcessStarter):
-        pattern = r"Running on http:"  # type: ignore
-        timeout = 2
-        terminate_on_interrupt = True
-        args = ["python",
-                "-m", "server"]  # type: ignore
-    logfile = xprocess.ensure(server_name, Starter)
-    yield dict(url="http://localhost", logfile=logfile)
-
-    xprocess.getinfo(server_name).terminate()
