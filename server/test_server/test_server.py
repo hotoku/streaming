414a6ab50ba83abc425870e4fb4978f1302c70a3
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-19 10:22:27 +0900

    type

--- server/test_server/test_server.py
+++ server/test_server/test_server.py
@@ -3,7 +3,7 @@ from flask.testing import FlaskClient
 
 def test_video_list(client: FlaskClient):
     ret = client.get("/video/list")
-    assert len(ret.json) == 100
+    assert ret.json is not None and len(ret.json) == 100
 
     ret2 = client.get("/video/list?num=20")
-    assert len(ret2.json) == 20
+    assert ret2.json is not None and len(ret2.json) == 20
