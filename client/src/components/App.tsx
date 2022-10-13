e29c31fddea9123cb178dd3fa9b2bc84179fe872
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-13 18:58:59 +0900

    error handling

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,19 +1,22 @@
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import ErrorBoundary from "./ErrorBoundary";
 import { Home } from "./Home";
 import { Play } from "./Play";
 
 export const App = (): JSX.Element => {
   return (
     <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/play/:id" element={<Play />} />
-          <Route path="/play/:id/path/:path" element={<Play />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
-      </BrowserRouter>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/play/:id" element={<Play />} />
+            <Route path="/play/:id/path/:path" element={<Play />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </BrowserRouter>
+      </ErrorBoundary>
     </RecoilRoot>
   );
 };
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ThumbnailList } from "../components/ThumbnailList";
 import { fetchVideoList } from "../db";
 import { useRecoilState } from "recoil";
@@ -7,11 +7,16 @@ import { videoListAtom } from "../atoms";
 
 export const Home = () => {
   const [videoList, setVideoList] = useRecoilState(videoListAtom);
+  const [isError, setIsError] = useState(false);
   useEffect(() => {
     if (videoList.length <= 0) {
-      console.log("new list");
-      fetchVideoList(setVideoList);
+      fetchVideoList(setVideoList).catch(() => {
+        setIsError(true);
+      });
     }
   }, []);
+  if (isError) {
+    throw Error("something is wrong");
+  }
   return ThumbnailList({ vs: videoList });
 };
