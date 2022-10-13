f4058f91239aee695f6561e347fdda8c3b25dc23
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-13 18:25:09 +0900

    wip

---
+++ release/readme.md
@@ -0,0 +1,11 @@
+# DBの構築
+
+```shell
+make -f build-db.mk db
+```
+
+# サービスの起動
+
+``` shell
+make -f release.mk load
+```
---
+++ release/release.mk
@@ -0,0 +1,40 @@
+DB_PATH := $(PWD)/db/db.sqlite
+RESOURCE_PATH := $(PWD)/resource
+STATIC_PATH := $(PWD)/static
+PORT=80
+
+
+DEST := ~/Library/LaunchAgents
+PLIST := info.hotoku.streaming.plist
+
+
+.PHONY: load
+load: $(PLIST) unload clear-log
+	cp $< $(DEST)
+	launchctl load $(DEST)/$<
+
+
+.PHONY: clean
+clean: unload clear-log
+	rm -f $(PLIST)
+
+
+.PHONY: unload
+unload:
+	launchctl unload $(DEST)/$(PLIST)
+	rm -f $(DEST)/$(PLIST)
+
+
+.PHONY: clear-log
+clear-log:
+	rm -f server.err server.out
+
+
+%: %.jinja
+	jinja2 -D pwd=$(PWD) \
+		-D label=$(subst .plist,,$(PLIST)) \
+		-D DB_PATH=$(DB_PATH) \
+		-D RESOURCE_PATH=$(RESOURCE_PATH) \
+		-D STATIC_PATH=$(STATIC_PATH) \
+		-D PORT=$(PORT) \
+		$< > $@
