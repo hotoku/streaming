3fc4350ef7b1eb114744c96fad81c438e7020ba5
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-14 23:36:44 +0900

    simplify release script

--- release/release.mk
+++ release/release.mk
@@ -4,47 +4,11 @@ export STATIC_PATH := $(PWD)/static
 export PORT=80
 
 
-DEST := ~/Library/LaunchAgents
-PLIST := info.hotoku.streaming.plist
-
-
-.PHONY: load
-load: $(PLIST) unload clear-log build
-	cp $< $(DEST)
-	launchctl load $(DEST)/$<
-
-
 .PHONY: buld
 build:
 	cd ../client && npm run build
 
 
-.PHONY: clean
-clean: unload clear-log
-	rm -f $(PLIST)
-
-
-.PHONY: unload
-unload:
-	launchctl unload $(DEST)/$(PLIST)
-	rm -f $(DEST)/$(PLIST)
-
-
-.PHONY: clear-log
-clear-log:
-	rm -f server.err server.out
-
-
-%: %.jinja
-	jinja2 -D pwd=$(PWD) \
-		-D label=$(subst .plist,,$(PLIST)) \
-		-D DB_PATH=$(DB_PATH) \
-		-D RESOURCE_PATH=$(RESOURCE_PATH) \
-		-D STATIC_PATH=$(STATIC_PATH) \
-		-D PORT=$(PORT) \
-		$< > $@
-
-
 .PHONY: start
-start:
+start: build
 	./run
