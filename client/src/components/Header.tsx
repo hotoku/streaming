9d8e907e0036a4bb54236082289980cc3388ca75
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-14 21:31:15 +0900

    header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,9 @@
 import Style from "./Header.module.css";
 
 export const Header = (): JSX.Element => {
-  return <div className={Style.header}>header</div>;
+  return (
+    <header>
+      <div className={Style.header}>header</div>
+    </header>
+  );
 };
