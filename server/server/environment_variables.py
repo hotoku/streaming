2d6273aa273cd4e51f62f5687bbb86ab18f77b61
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-18 06:57:27 +0900

    uploaded_path dir will be made automatically if not exists.
    db, resource paths should be generated outside of server process.

--- server/server/environment_variables.py
+++ server/server/environment_variables.py
@@ -5,6 +5,10 @@ db_path: str = os.getenv("DB_PATH")  # type: ignore
 resource_path: str = os.getenv("RESOURCE_PATH")  # type: ignore
 uploaded_path: str = os.getenv("UPLOADED_PATH")  # type: ignore
 
+if not os.path.exists(uploaded_path):
+    os.makedirs(uploaded_path, exist_ok=True)
+
+
 static_path: Optional[str] = os.getenv("STATIC_PATH")
 port: int = int(os.getenv("PORT") or 8080)
 
