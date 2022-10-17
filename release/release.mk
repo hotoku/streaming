41a6e7683862310311e4c546307d89812981bd62
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-18 06:58:18 +0900

    fix release scripts

--- release/release.mk
+++ release/release.mk
@@ -1,10 +1,6 @@
-ifeq ($(TEST),true)
-export DB_PATH := $(PWD)/../server/db/db.sqlite
-export RESOURCE_PATH := $(PWD)/../server/resource
-else
 export DB_PATH := $(PWD)/db/db.sqlite
 export RESOURCE_PATH := $(PWD)/resource
-endif
+export UPLOADED_PATH := $(PWD)/resource/uploaded
 
 
 export STATIC_PATH := $(PWD)/static
