b5039982cbb48218b38ea135d051756fe2fe8d60
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-01-06 16:17:10 +0900

    fix

--- release/build-db.mk
+++ release/build-db.mk
@@ -1,8 +1,6 @@
 export INFO_JSON := $(PWD)/.info.json
 export DB_PATH := $(PWD)/db/db.sqlite
 export RESOURCE_PATH := $(PWD)/resource
-export STATIC_PATH := $(PWD)/static
-export PORT=80
 
 
 VIDEO_DIR = $(RESOURCE_PATH)/videos
@@ -28,7 +26,7 @@ db: thumbnails
 
 .PHONY: thumbnails
 thumbnails:
-	cd ../server && python -m thumbnail \
+	cd ../server && poetry run python -m thumbnail \
 		$(PWD)/prefix.json $(VIDEO_DIR) $(THUMBNAIL_DIR) $(INFO_JSON) \
 		--pos "00:00:20.000"
 
