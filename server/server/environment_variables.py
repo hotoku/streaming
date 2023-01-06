1f11b68c8255d30c313149aa9ec649a49b4a91a5
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-01-06 16:08:53 +0900

    env vars

--- server/server/environment_variables.py
+++ server/server/environment_variables.py
@@ -5,13 +5,13 @@ resource_path: str = os.getenv("RESOURCE_PATH")  # type: ignore
 uploaded_path: str = os.getenv("UPLOADED_PATH")  # type: ignore
 root_path: str = os.getenv("ROOT_PATH") or os.getcwd()
 
+assert db_path is not None
+assert resource_path is not None
+assert uploaded_path is not None
+
+
 if not os.path.exists(uploaded_path):
     os.makedirs(uploaded_path, exist_ok=True)
 
 
 port: int = int(os.getenv("PORT") or 8080)
-
-
-assert db_path is not None
-assert resource_path is not None
-assert uploaded_path is not None
